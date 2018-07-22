2d96397c0b8d28b8ed06dcb99aee4003f93d3e5f
Xiaodong Li <bryne_lxd@sina.com>
2018-07-22 21:44:06 +0800

    state:2500

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -17,6 +17,12 @@ describe('LearnJS',function(){
 		expect(learnjs.problemView).toHaveBeenCalledWith('42');
 	});
 
+	it('involves the router when loaded', function(){
+		spyOn(learnjs, 'showView');
+		learnjs.appOnReady();
+		expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+	});
+
 	describe('problem view', function(){
 		it('has a title that includes the problem number', function(){
 			var view = learnjs.problemView('1');
