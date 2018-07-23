fe9819211d77cb40d46ff78bec6102521fefe75f
Xiaodong Li <bryne_lxd@sina.com>
2018-07-24 00:21:41 +0800

    state:3300

--- public/app.js
+++ public/app.js
@@ -1,10 +1,50 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber){
-	var title = 'Problem #' + problemNumber + ' Coming soon!';
-	return $('<div class="problem-view">').text(title);
-}
+learnjs.problems = [
+	{
+		description: "What is truth?",
+		code: "function problem() { return __; }"
+	},
+	{
+		description: "Simple Math",
+		code: "function problem() { return 42 === 6 * __; }"
+	}
+];
+
+learnjs.applyObject = function(obj, elem) {
+	for (var key in obj) {
+		elem.find('[data-name="' + key + '"]').text(obj[key]);
+	}
+};
+
+learnjs.problemView = function(data){
+	var problemNumber = parseInt(data, 10);
+	var view = $('.templates .problem-view').clone();
+	var problemData = learnjs.problems[problemNumber - 1];
+	var resultFlash = view.find('.result');
+
+	function checkAnswer() {
+		var answer = view.find('.answer').val();
+		var test = problemData.code.replace('__', answer) + '; problem();';
+		return eval(test);
+    }
+
+    function checkAnswerClick() {
+		if (checkAnswer()){
+			resultFlash.text('Correct!');
+		}else{
+			resultFlash.text('Incorrect!');
+		}
+		return false;
+	}
+
+
+	view.find('.check-btn').click(checkAnswerClick);
+	view.find('.title').text('Problem #'+problemNumber);
+	learnjs.applyObject(problemData, view);
+	return view;
+};
 
 learnjs.showView = function(hash){
 	var routes = {
@@ -15,11 +55,11 @@ learnjs.showView = function(hash){
 	if(viewFn){
 		$('.view-container').empty().append(viewFn(hashParts[1]));
 	}
-}
+};
 
 learnjs.appOnReady = function(){
 	window.onhashchange = function(){
 		learnjs.showView(window.location.hash);
 	};
 	learnjs.showView(window.location.hash);
-}
+};
