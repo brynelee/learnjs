5057085ba67eef48ad1968744783e44204bf324d
Xiaodong Li <bryne_lxd@sina.com>
2018-07-24 00:35:56 +0800

    state:3500

--- public/app.js
+++ public/app.js
@@ -18,6 +18,17 @@ learnjs.applyObject = function(obj, elem) {
 	}
 };
 
+learnjs.flashElement = function(elem, content){
+	elem.fadeOut('fast', function(){
+		elem.html(content);
+		elem.fadeIn();
+	});
+};
+
+learnjs.template = function(name){
+	return $('.templates .' + name).clone();
+}
+
 learnjs.problemView = function(data){
 	var problemNumber = parseInt(data, 10);
 	var view = $('.templates .problem-view').clone();
@@ -32,9 +43,11 @@ learnjs.problemView = function(data){
 
     function checkAnswerClick() {
 		if (checkAnswer()){
-			resultFlash.text('Correct!');
+			var correctFlash = learnjs.template('correct-flash');
+			correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+			learnjs.flashElement(resultFlash, correctFlash);
 		}else{
-			resultFlash.text('Incorrect!');
+			learnjs.flashElement(resultFlash, 'Incorrect!');
 		}
 		return false;
 	}
